267da4abe48094344541a65588f66dcb2f6f5531
joshua-arnao <joshua.arnao@icloud.com>
2025-07-28 10:39:04 -0500

    chore: reorganize project folder structure

---
+++ src/main/java/mp3musicplayer/App.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mp3musicplayer;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author joshuaarnaocanessa
+ */
+public class App {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable(){
+            @Override
+            public void run(){
+                new MusicPlayerGUI().setVisible(true);
+            }
+        
+        });
+        
+
+    }
+    
+
+}
